8c272012a0ac9dfa71c7288b078fc50b283fbfa1
rpol <polrotyuni@gmail.com>
2022-02-21 14:18:30 +0100

    philo start

---
+++ philo/includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpol <rpol@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 14:03:52 by rpol              #+#    #+#             */
+/*   Updated: 2022/02/21 14:14:33 by rpol             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+/*IN PHILO.C*/
+
+int	philo(int ac, char **av);
+
+#endif
